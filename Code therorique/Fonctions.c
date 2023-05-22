86ab9d1f0e2d203a87ae3949f65b75783b9c485f
Tarhken <134166642+Tarhken@users.noreply.github.com>
2023-05-22 23:53:25 +0200

    Update Fonctions.c
    Modif code

--- Code therorique/Fonctions.c
+++ Code therorique/Fonctions.c
@@ -1,24 +1,37 @@
 #include "Fonctions.h"
 #include <stdlib.h>
-int* Initialisation(int lignes, int colonnes) {
-	int* tab;
-	tab = malloc(lignes * sizeof(int) + colonnes * sizeof(int));
+#include <stdio.h>
+int** Initialisation(int lignes, int colonnes) {
+	int k;
+	int** tab;
+	tab = malloc(lignes * sizeof(int));
+	for (k = 0; k < lignes; k++) {
+		tab[k] = malloc(colonnes * sizeof(int));
+	}
 	int i, j;
 	for (i = 0; i < lignes; i++) {
 		for (j = 0; j < colonnes; j++) {
-			*(tab + i * colonnes * sizeof(int) + j * sizeof(int)) = 0;
+			tab[i][j] = 0;
 		}
 	}
 	return tab;
 }
 
-void AffichageTableau(int *tab, int lignes, int colonnes) {
+void AffichageTableau(int** tab, int lignes, int colonnes) {
 	int i, j;
 	for (i = 0; i < lignes; i++) {
 		for (j = 0; j < colonnes; j++) {
-			printf("%d", tab + i * colonnes + j);
+			printf("%d", tab[i][j]);
 		}
 		printf("\n");
 	}
 
+}
+
+void SuppressionTab(int** tab, int lignes) {
+	int i;
+	for (i = 0; i < lignes; i++) {
+		free(tab[i]);
+	}
+	free(tab);
 }
